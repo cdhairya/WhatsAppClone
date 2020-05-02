669e49a0920026d7c3528af43cb99dded5bbb345
Dhairya Chheda <dhairya.chheda@somaiya.edu>
2020-05-02 12:09:45 +0530

    Using Firebase Realtime database for adding users tha log in for first time.

--- app/src/main/java/com/example/whatsappclone/LoginActivity.java
+++ app/src/main/java/com/example/whatsappclone/LoginActivity.java
@@ -18,10 +18,17 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     EditText etPhone, etCode;
     Button btnSend;
@@ -79,7 +86,31 @@ public class MainActivity extends AppCompatActivity {
         FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()) userIsLoggedIn();
+                if(task.isSuccessful()) {
+
+                    final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    if(user != null){
+                        final DatabaseReference UserDB = FirebaseDatabase.getInstance().getReference().child("user").child(user.getUid());
+                        UserDB.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if(!dataSnapshot.exists()){
+                                    Map<String, Object> userMap = new HashMap<>();
+                                    userMap.put("phone",user.getPhoneNumber());
+                                    userMap.put("name", user.getPhoneNumber());
+                                    UserDB.updateChildren(userMap);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+
+                    //userIsLoggedIn();
+                }
             }
         });
     }
