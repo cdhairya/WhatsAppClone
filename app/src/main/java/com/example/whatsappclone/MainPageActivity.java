4c80031a22fdb338ef400a581619c1d054baca13
Dhairya Chheda <dhairya.chheda@somaiya.edu>
2020-05-02 11:42:07 +0530

    Added Phone number verification using Firebase Auth and main page activity once verification is successful.

---
+++ app/src/main/java/com/example/whatsappclone/MainPageActivity.java
@@ -0,0 +1,51 @@
+package com.example.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    Button btnLogout, btnFindUser;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        btnLogout = findViewById(R.id.btnLogout);
+        btnFindUser = findViewById(R.id.btnFindUser);
+
+        btnFindUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FindUserActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+        getPermissions();
+    }
+
+    private void getPermissions() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            requestPermissions(new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS},1);
+    }
+}
