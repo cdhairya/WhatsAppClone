ccd4f058da59e8fb1ae20a4ec03a56df33c128ce
Dhairya Chheda <dhairya.chheda@somaiya.edu>
2020-05-03 11:07:04 +0530

    Setting up recycler view for displaying chat list of user.

---
+++ app/src/main/java/com/example/whatsappclone/Chat/ChatListAdapter.java
@@ -0,0 +1,64 @@
+package com.example.whatsappclone.Chat;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.whatsappclone.R;
+import com.example.whatsappclone.Chat.ChatObject;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.ChatListViewHolder> {
+
+    ArrayList<ChatObject> chats;
+
+    public ChatListAdapter(ArrayList<ChatObject> users) {
+        this.chats = chats;
+    }
+
+    @NonNull
+    @Override
+    public ChatListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_chat, null, false);
+        RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutView.setLayoutParams(lp);
+        ChatListViewHolder rcv = new ChatListViewHolder(layoutView);
+        return rcv;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ChatListViewHolder holder, final int position) {
+        holder.mTitle.setText(chats.get(position).getChatId());
+
+        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return chats.size();
+    }
+
+    public class ChatListViewHolder extends RecyclerView.ViewHolder{
+        public TextView mTitle;
+        public LinearLayout linearLayout;
+        public ChatListViewHolder(View view) {
+            super(view);
+            mTitle = view.findViewById(R.id.title);
+            linearLayout = view.findViewById(R.id.linearLayout);
+        }
+    }
+}
--- app/src/main/java/com/example/whatsappclone/MainPageActivity.java
+++ app/src/main/java/com/example/whatsappclone/MainPageActivity.java
@@ -1,6 +1,9 @@
 package com.example.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.content.Intent;
@@ -9,9 +12,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.whatsappclone.Chat.ChatListAdapter;
+import com.example.whatsappclone.Chat.ChatObject;
+import com.example.whatsappclone.User.UserListAdapter;
+import com.example.whatsappclone.User.UserObject;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainPageActivity extends AppCompatActivity {
+
+    private RecyclerView listViewChat;
+    private RecyclerView.Adapter chatListAdapter;
+    private RecyclerView.LayoutManager chatListLayoutManager;
+
+    ArrayList<ChatObject>  chatList;
 
     Button btnLogout, btnFindUser;
     @Override
@@ -42,8 +62,47 @@ public class MainPageActivity extends AppCompatActivity {
             }
         });
         getPermissions();
+        initializeRecyclerView();
+        getUserChatList();
     }
 
+    private void getUserChatList(){
+        DatabaseReference UserChatDB = FirebaseDatabase.getInstance().getReference().child("user").child(FirebaseAuth.getInstance().getUid()).child("chat");
+        UserChatDB.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot childSnapshot: dataSnapshot.getChildren()){
+                        ChatObject Chat = new ChatObject(childSnapshot.getKey());
+                        boolean exists = false;
+                        for(ChatObject ChatIterator: chatList){
+                            if(ChatIterator.getChatId().equals(Chat.getChatId()))
+                                exists = true;
+                        }
+                        if(exists)
+                            continue;
+                        chatList.add(Chat);
+                        chatListAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void initializeRecyclerView() {
+        listViewChat = findViewById(R.id.listViewChat);
+        listViewChat.setNestedScrollingEnabled(false);
+        listViewChat.setHasFixedSize(false);
+        chatListLayoutManager = new LinearLayoutManager(getApplicationContext(), RecyclerView.VERTICAL, false);
+        listViewChat.setLayoutManager(chatListLayoutManager);
+        chatListAdapter = new ChatListAdapter(chatList);
+        listViewChat.setAdapter(chatListAdapter);
+    }
     private void getPermissions() {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
             requestPermissions(new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS},1);
