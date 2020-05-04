5faac2d8190c3b90749af128f579ee7dcafbc114
Dhairya Chheda <dhairya.chheda@somaiya.edu>
2020-05-04 21:39:28 +0530

    Messages sent are being displayed correctly along with the sender's unique uid.

--- app/src/main/java/com/example/whatsappclone/ChatActivity.java
+++ app/src/main/java/com/example/whatsappclone/ChatActivity.java
@@ -1,5 +1,7 @@
 package com.example.whatsappclone;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +15,9 @@ import com.example.whatsappclone.Chat.ChatObject;
 import com.example.whatsappclone.Chat.MessageAdapter;
 import com.example.whatsappclone.Chat.MessageObject;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -28,12 +33,15 @@ public class ChatActivity extends AppCompatActivity {
 
     ArrayList<MessageObject> messageList;
     String chatID;
+    DatabaseReference chatDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
         chatID = getIntent().getExtras().getString("chatID");
+
+        chatDB = FirebaseDatabase.getInstance().getReference().child("chat").child(chatID);
 
         Button send = findViewById(R.id.send);
         send.setOnClickListener(new View.OnClickListener() {
@@ -43,12 +51,49 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
         initializeRecyclerView();
+        getChatMessages();
+    }
+
+    private void getChatMessages() {
+        chatDB.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                if(dataSnapshot.exists()){
+                    String text = "", creatorID = "";
+                    if(dataSnapshot.child("text").getValue() != null)
+                        text = dataSnapshot.child("text").getValue().toString();
+                    if(dataSnapshot.child("creator").getValue() != null)
+                        creatorID = dataSnapshot.child("creator").getValue().toString();
+
+                    MessageObject myMessage = new MessageObject(dataSnapshot.getKey(),creatorID,text);
+                    messageList.add(myMessage);
+                    chatLayoutManager.scrollToPosition(messageList.size() - 1);
+                    chatAdapter.notifyDataSetChanged();
+                }
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void sendMessage() {
         EditText etMessage = findViewById(R.id.etMessage);
         if(!etMessage.getText().toString().isEmpty()){
-            DatabaseReference newMessageDB = FirebaseDatabase.getInstance().getReference().child("chat").child(chatID).push();
+            DatabaseReference newMessageDB = chatDB.push();
             Map newMessageMap = new HashMap<>();
             newMessageMap.put("text",etMessage.getText().toString());
             newMessageMap.put("creator", FirebaseAuth.getInstance().getUid());
